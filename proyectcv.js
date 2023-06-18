c4b811cab9265b4bf4467c1d3bf386d964f46de3
Alexander <alexhellhammer@hotmail.com>
2023-06-17 23:39:22 -0500

    implementacion de JS en el menu

--- proyectcv.js
+++ proyectcv.js
@@ -1,16 +1,38 @@
 let menuVisible = false;
-
-// funcion que oculta o muestra el menu
+//Función que oculta o muestra el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
-        document.getElementById("nav").classList="";
-        menuVisible = false
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
     }else{
-        document.getElementById("nav").classList="responsive";
-        menuVisible = true
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
     }
-
+}
+function seleccionar(){
+    // oculto el menu cuando selecciono una opcion
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+// funcion qu aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("Hmtl");
+        habilidades[2].classList.add("Css");
+        habilidades[3].classList.add("Php");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
+       
+    }
 }
 
-
-
+// efecto scroll para aplicar la animacion de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+} 
